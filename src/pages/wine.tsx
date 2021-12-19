2e12488441cc51c89c03e6d29786eb8f4ac74509
charile1 <pyo05360@gmail.com>
2021-12-19 21:26:48 +0900

    1day

--- src/pages/wine.tsx
+++ src/pages/wine.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-// NextPage은 타입을 선언해준것. 타입스크립트 가서 더 배우자 .
+// NextPage은 타입을 선언해준것. 타입스크립트 가서 더 배우자.
 // import 가 뭔가요? 몰라도 됌 
 
 // 타입 선언 
@@ -13,4 +13,4 @@ const WinePage: NextPage = () => {
 
 export default WinePage;
 // export 가 뭔가요? 몰라도 됌 
-// 여기까지가 가장 기본 적인 템플릿이다.
+// 여기까지가 가장 기본적인 템플릿이다.
